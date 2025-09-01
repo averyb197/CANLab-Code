2788635b5fa3672760cdcfd0b498380b7cbfe136
Brady <bflanagan4@luc.edu>
2025-09-01 16:54:23 -0500

    adding DRAT Stim List

--- DRAT_workingfile.m
+++ DRAT_workingfile.m
@@ -4,6 +4,7 @@ close all;
 clear;
 clc;
 
+Screen('Preference', 'SkipSyncTests', 1);
 screenNumber=max(Screen('Screens'));
 
 %Define colors
@@ -19,33 +20,45 @@ w=Screen('OpenWindow', screenNumber, black);
 Screen('TextSize',w, 50);
 
 %Restrict keys to A, L, or P
-targetKeys = [KbName('A'), KbName('L'), KbName('P')];
-RestrictKeysForKbCheck(targetKeys);
-responses=zeros(1,10);
+% targetKeys = [KbName('A'), KbName('L'), KbName('P')];
+% RestrictKeysForKbCheck(targetKeys);
+responses=zeros(1, 75);
 
+%Text with Instructions
+DrawFormattedText(w, 'hi this is test', 'center', 'center', white);
+Screen('Flip', w);
+KbWait();
+WaitSecs(0.5);
 
 %Creating and Pasting a Sequence of 10 random sets of 4 letters
-for i= 1:10
+for i= 1:15   
 
     %Generate Sequence of Letters
     randIdx = randperm(length(alphabet), 4);
     rand_letters = alphabet(randIdx);
     DrawFormattedText(w, rand_letters, 'center', 'center', white);
     Screen('Flip', w);
-    WaitSecs(0.5);
+    WaitSecs(3);
 
     %Crosshair pause time
     DrawFormattedText(w, '+', 'center', 'center', white);
     Screen('Flip', w);
-    WaitSecs(0.5);
+    WaitSecs(5);
 
-    %Stimuli of position and letter, as for response (A, L, P)
-    DrawFormattedText(w, ['3\n' ...
-        'L'], 'center', 'center', white);
-    Screen('Flip', w);
-    [secs, keys]=KbWait();
-    responses(i)=find(keys);
-    WaitSecs(0.5);
+    % for j=1:5
+    %     %Stimuli of position and letter, as for response (A, L, P)
+    %     DrawFormattedText(w, ['3\n' ...
+    %         'L'], 'center', 'center', white);
+    %     Screen('Flip', w);
+    %     [secs, keys]=KbWait();
+    %     responses(i)=find(keys);
+    %     WaitSecs(0.5);
+    % 
+    %     %Crosshair pause time
+    %     DrawFormattedText(w, '+', 'center', 'center', white);
+    %     Screen('Flip', w);
+    %     WaitSecs(0.5);
+    % end
 end
 
 
